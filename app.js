7923de010c6f27ea0315d140e1a9a1f128017e18
Dominic DeJesus <domludejesus@gmail.com>
2022-05-31 22:53:47 -0400

    'comments'

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+// inventory (change to zero to see example of failure)
 const inventory = {
   sunglasses: 1900,
   pants: 1088,
   bags: 1344
 };
 
+// promise of ordering sunglasses from beach store 
 const myExecutor = (resolve, reject) => {
     if (inventory.sunglasses > 0) {
         resolve('Sunglasses order processed.'); 
@@ -11,11 +13,12 @@ const myExecutor = (resolve, reject) => {
           reject('That item is sold out.');
       }
     }
-    
+    // promise constructor 
     const orderSunglasses = () => {
         return new Promise(myExecutor);
     };
-    
+    // returned value from orderSunglasses 
     const orderPromise = orderSunglasses();
     
+    // use node app.js to test code
     console.log(orderPromise)
